75bbfbceb8adb02bb5829ada2ea2052e3a85cf6b
Claudiu COTAN <claudiu@cotan.fr>
2020-05-18 16:35:55 +0200

    Created HTML index structure.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>homepage</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+    </head>
+    <body>
+        <h1>Sites List</h1>
+        <p>Here you can see you sites list</p>
+        <footer>
+            <p><a href="https://github.com/claudiucotan/homepage">Fork me</a> on Github</p>
+        </footer>
+    </body>
+
+</html>
