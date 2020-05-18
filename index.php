1913993eeb8f623bdb4029c4986086e8822a7afa
Claudiu COTAN <claudiu@cotan.fr>
2020-05-18 16:42:51 +0200

    Fixed the self closing tag of stylesheet.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/style.css" />
     </head>
     <body>
         <h1>Sites List</h1>
