70bd6adde79fe62bbb5fa2d1cd42a208066fbb55
Claudiu COTAN <claudiu@cotan.fr>
2020-05-18 16:41:02 +0200

    Added css file and linked it to index.

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
+
 <html>
     <head>
         <title>homepage</title>
         <meta charset="utf-8" />
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <h1>Sites List</h1>
