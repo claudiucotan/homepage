5285b7306f78cb7dca87ce57481afdb8570d49ac
Claudiu COTAN <claudiu@cotan.fr>
2020-05-18 18:25:47 +0200

    Added some comments for clarification.

--- config.php
+++ config.php
@@ -1,4 +1,16 @@
 <?php
+
+/**
+*  
+*  This is the config file to store your variables
+*  
+*  Change "/Users/claudiucotan/Sites" to the directory where you keep your sites. 
+*  
+*  
+*  
+*/
+
+
 $directory = "/Users/claudiucotan/Sites";
 $recursively = $directory . "/*";
 ?>
