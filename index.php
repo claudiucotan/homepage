fb87dc5f2a9fa1cf13b12819363c287ceee8fc88
Claudiu COTAN <claudiu@cotan.fr>
2020-05-22 13:37:38 +0200

    Changed the location of files for better integration.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <!-- Calling the config file -->
-<?php require('config.php'); ?>
+<?php require('homepage/config.php'); ?>
 <!-- ----------------------- -->
 <html>
     <head>
@@ -8,7 +8,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="homepage/css/style.css" />
     </head>
     <body>
         <div class="container">
